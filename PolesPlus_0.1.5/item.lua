53a58a44cba0f3b0320df2f1b07520ac0cb7729a
uzayyli <87779551+uzayyli@users.noreply.github.com>
2025-12-01 04:48:34 +0300

    v0.1.5
    Also sets underground belt max distance equal to fast underground belt (7 tiles). Blue (express) underground belts are 9 tiles
    Reduced small pole supply area from 16 to 8

---
+++ PolesPlus_0.1.5/item.lua
@@ -0,0 +1,33 @@
+local lights_on=settings.startup["poles-plus-lighted-poles"].value;
+local light_params=nil;
+if(lights_on) then
+light_params={intensity = settings.startup["poles-plus-light-intensity"].value, size = settings.startup["poles-plus-light-size"].value, color = {r=1.0, g=1.0, b=1.0}}
+end
+
+local p=data.raw['electric-pole']['small-electric-pole'];
+local pr=settings.startup["poles-plus-small-reach"].value;
+p.maximum_wire_distance = pr;
+p.supply_area_distance = pr/2;
+if(lights_on) then p.light=light_params; end
+
+p=data.raw['electric-pole']['medium-electric-pole'];
+pr=settings.startup["poles-plus-medium-reach"].value;
+p.maximum_wire_distance = pr;
+p.supply_area_distance = pr/2;
+if(lights_on) then p.light=light_params; end
+
+p=data.raw['electric-pole']['big-electric-pole'];
+pr=settings.startup["poles-plus-big-reach"].value;
+p.maximum_wire_distance = pr;
+if(lights_on) then p.light=light_params; end
+
+p=data.raw['electric-pole']['substation'];
+pr=settings.startup["poles-plus-sub-reach"].value;
+p.maximum_wire_distance = pr;
+p.supply_area_distance = pr/2;
+if(lights_on) then p.light=light_params; end
+
+p=data.raw['pipe-to-ground']['pipe-to-ground'];
+p.fluid_box.pipe_connections[2].max_underground_distance=settings.startup["poles-plus-underground-length"].value;
+
+if(settings.startup["longer-yellow-underground-belt"].value) then data.raw['underground-belt']['underground-belt'].max_distance = data.raw['underground-belt']['fast-underground-belt'].max_distance; end
